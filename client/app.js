6bca8929d11f33a304919851b4ae2650475fd619
Maria Gozdawa-Gołębiowska <m.gozdawa-golebiowska@gmail.com>
2024-09-29 16:36:17 +0200

    add socket

--- client/app.js
+++ client/app.js
@@ -1,42 +1,51 @@
+const socket = io();
+const loginForm = document.getElementById("welcome-form");
+const messagesSection = document.getElementById("messages-section");
+const messagesList = document.getElementById("messages-list");
+const addMessageForm = document.getElementById("add-messages-form");
+const userNameInput = document.getElementById("username");
+const messageContentInput = document.getElementById("message-content");
 
-const loginForm = document.getElementById('welcome-form');
-const messagesSection = document.getElementById('messages-section');
-const messagesList = document.getElementById('messages-list');
-const addMessageForm = document.getElementById('add-messages-form');
-const userNameInput = document.getElementById('username');
-const messageContentInput = document.getElementById('message-content');
+let userName = "";
 
-let userName = '';
+socket.on("message", ({ author, content }) => addMessage(author, content));
+socket.on("newUser", (userName) => {
+  addMessage("Chat Bot", userName + " dołączył do czatu:)", true);
+});
+socket.on("userLeft", (userName) => {
+  addMessage("Chat Bot", userName + " opuścił czat... :(", true);
+});
+function login(event) {
+  event.preventDefault();
 
-const login = function(event) {
-  event.preventDefault(); 
-  
+  const username = userNameInput.value.trim();
 
-  if (userNameInput.value.trim() === '') {
-    alert('Podaj nazwę uzytkownika!');
-  } else {
-    userName = userNameInput.value.trim(); 
-    
-    loginForm.classList.remove('show');
-    
-    messagesSection.classList.add('show');
+  if (username === "") {
+    alert("Nazwa użytkownika nie może być pusta!");
+    return;
   }
-};
+  userName = username;
+  socket.emit("join", userName);
+  loginForm.classList.remove("show");
 
-loginForm.addEventListener('submit', login);
+  messagesSection.classList.add("show");
+
+  console.log("Witaj,", userName);
+}
+loginForm.addEventListener("submit", login);
 
 function sendMessage(event) {
   event.preventDefault();
 
   const messageInput = messageContentInput.value;
   if (!messageInput.length) {
-    alert("Wpisz treść wiadomości");
+    alert("Pole nie moe być puste!");
   } else {
     addMessage(userName, messageInput);
+    socket.emit("message", { author: userName, content: messageInput });
     messageContentInput.value = "";
   }
 }
-
 addMessageForm.addEventListener("submit", sendMessage);
 
 function addMessage(author, content) {
@@ -44,6 +53,9 @@ function addMessage(author, content) {
   message.classList.add("message");
   message.classList.add("message--received");
   if (author === userName) message.classList.add("message--self");
+  if (author === "Chat Bot") {
+    message.classList.add("message--bot");
+  }
   message.innerHTML = `
       <h3 class="message__author">${userName === author ? "You" : author}</h3>
       <div class="message__content">
@@ -52,3 +64,6 @@ function addMessage(author, content) {
     `;
   messagesList.appendChild(message);
 }
+
+userNameInput.setAttribute("autoComplete", "off");
+messageContentInput.setAttribute("autoComplete", "off");
--- server.js
+++ server.js
@@ -1,12 +1,45 @@
 const express = require("express");
 const path = require("path");
-
+const socket = require("socket.io");
 const app = express();
 
-app.listen(8000, () => {
+const server = app.listen(8000, () => {
   console.log("Server is running on port: 8000");
 });
+const io = socket(server);
 const messages = [];
+const user = [];
+
+io.on("connection", (socket) => {
+  console.log("New client! Its id – " + socket.id);
+
+  socket.on("join", (userName) => {
+    user.push({ name: userName, id: socket.id });
+    console.log("User " + userName + " with id " + socket.id + " has joined.");
+    console.log("Current users", user);
+    socket.broadcast.emit("newUser", userName);
+  });
+
+  socket.on("message", (message) => {
+    console.log("Oh, I've got something from " + socket.id);
+    messages.push(message);
+    socket.broadcast.emit("message", message);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Oh, socket " + socket.id + " has left");
+    const index = user.findIndex((user) => user.id === socket.id);
+    if (index !== -1) {
+      const userName = user[index].name;
+      console.log("User " + userName + " with id " + socket.id + " has left");
+      user.splice(index, 1);
+      console.log("Current users:", user);
+      socket.broadcast.emit("userLeft", userName);
+    }
+  });
+
+  console.log("I've added a listener on message and disconnect events \n");
+});
 
 app.use(express.static(path.join(__dirname, "/client")));
 app.get("*", (req, res) => {
